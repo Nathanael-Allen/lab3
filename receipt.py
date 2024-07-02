86144eba1bedf91978bd43887becd3fd2049bb0b
nathanaelA <nathanael.allen1@gmail.com>
2024-07-01 17:27:28 -0700

    minor formatting change

--- receipt.py
+++ receipt.py
@@ -1,11 +1,11 @@
 def printReceipt(total, tip, items):
-    print('-' * 10)
+    print('-' * 15)
     print('RECEIPT')
-    print('-' * 10)
+    print('-' * 15)
 
     for item in items:
         print(f'{item['item']}: ${item['cost']:.2f}')
-        print('-' * 10)
+        print('-' * 15)
 
     print(f'Tip: ${tip:.2f}')
     print(f'Total: ${total:.2f}')
